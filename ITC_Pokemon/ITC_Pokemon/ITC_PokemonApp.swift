ed55f91fe99e4ffb9685980c2a29114fb05eed27
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-25 09:04:14 +0100

    Initial commit

---
+++ ITC_Pokemon/ITC_Pokemon/ITC_PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITC_PokemonApp.swift
+//  ITC_Pokemon
+//
+//  Created by Taijaun Pitt on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ITC_PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
