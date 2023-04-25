ed4926bcb3732d20a9397d47405c84dd5eb7f5c8
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-25 10:06:47 +0100

    Created method to call api in network manager

--- ITC_Pokemon/ITC_Pokemon/Services/NetworkManager.swift
+++ ITC_Pokemon/ITC_Pokemon/Services/NetworkManager.swift
@@ -11,6 +11,14 @@ class NetworkManager: Networker {
     
     func callApi(url: URL) async throws -> Data {
         
+        // Structured concurrency
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return data
+        } catch let error{
+            throw error
+        }
+        
     }
     
     
