626ddddbbd171a200f9e6ffac60341c2bff37818
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-25 09:33:25 +0100

    added apiendpoints struct

---
+++ ITC_Pokemon/ITC_Pokemon/Model/APIEndpoints.swift
@@ -0,0 +1,13 @@
+//
+//  APIEndpoints.swift
+//  ITC_Pokemon
+//
+//  Created by Taijaun Pitt on 25/04/2023.
+//
+
+import Foundation
+
+struct APIEndpoints {
+    
+    static let pokemonListEndpoint = "https://api.pokemontcg.io/v2/cards?page=1&pageSize=15"
+}
