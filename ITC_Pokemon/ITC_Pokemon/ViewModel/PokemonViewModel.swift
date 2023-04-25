517a930ecd6801d1275a250631f85318bb9b82f4
mo khaife <muhammad.a.ali88@gmail.com>
2023-04-25 11:14:00 +0100

    able to call data

--- ITC_Pokemon/ITC_Pokemon/ViewModel/PokemonViewModel.swift
+++ ITC_Pokemon/ITC_Pokemon/ViewModel/PokemonViewModel.swift
@@ -10,7 +10,34 @@ import Foundation
 class PokemonViewModel: ObservableObject {
     @Published var pokemonList = [PokemonDetails]()
     
-    func getListOfPokemons(urlString: String){
+    // initialize Nwetwork
+    var manager: Networker
+    
+    init(manager: Networker) {
+        self.manager = manager
+    }
+    
+    func getListOfPokemons(urlString: String) async{
+        // convert URL string to URL
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        do {
+            // initialize async await func to fetch data from Network Manager
+            let data = try await self.manager.callApi(url: url)
+            // decode the JSON from raw data
+            let pokemonData = try JSONDecoder().decode(Pokemon.self, from: data)
+            // make your publishable Model match the data returned
+            self.pokemonList = pokemonData.data
+            print(self.pokemonList)
+            
+        }catch let error{
+            print(error.localizedDescription)
+            
+            
+        }
+        
+        
         
     }
 }
