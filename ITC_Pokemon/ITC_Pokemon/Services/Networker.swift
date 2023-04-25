c6a9d5c1a361f7c661972cec183327372c23ac15
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-25 09:48:39 +0100

    Added networker protocol

---
+++ ITC_Pokemon/ITC_Pokemon/Services/Networker.swift
@@ -0,0 +1,14 @@
+//
+//  Networker.swift
+//  ITC_Pokemon
+//
+//  Created by Taijaun Pitt on 25/04/2023.
+//
+
+import Foundation
+
+protocol Networker {
+    
+    func callApi(urlString: String)
+    
+}
